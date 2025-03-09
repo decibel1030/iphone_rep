42c8068932cef78c10d17ad3f4f5bf5ba4447693
decibel1030 <ernurdenasan@gmail.com>
2025-03-09 22:32:55 +0500

    end

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
+import Features from "./components/Features"
+import Footer from "./components/Footer"
+import * as Sentry from "@sentry/react";
+
+
 function App() {
   return (
     <main className="bg-black">
@@ -9,8 +14,10 @@ function App() {
       <Hero/>
       <Highlights/>
       <Model/>
+      <Features/>
+      <Footer/>
     </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
 import './index.css'
 import App from './App.jsx'
+
+import * as Sentry from "@sentry/react";
+
+
+Sentry.init({
+  dsn: "https://cb4d897af99c8913a3abb1d22fc16024@o4508948478361600.ingest.us.sentry.io/4508948480000000",
+  integrations: [Sentry.browserTracingIntegration()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+});
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
